76fa6fd04c6905bf541958590ef2dd873ab95e81
Nick Satterly <nick.satterly@guardian.co.uk>
2013-05-01 12:29:47 -0500

    add standard key value attributes to user-defined ones

--- gmetad/export_helpers.c
+++ gmetad/export_helpers.c
@@ -354,7 +354,6 @@ write_data_to_carbon ( const char *source, const char *host, const char *metric,
 }
 
 #ifdef WITH_RIEMANN
-
 #define STATIC_ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 char **strtoknize(const char *str, size_t slen, const char *dlmt, int dlen, int  *n_toks)
@@ -388,7 +387,6 @@ char **strtoknize(const char *str, size_t slen, const char *dlmt, int dlen, int 
         return buffer;
 }
 
-
 int
 send_data_to_riemann (const char *grid, const char *cluster, const char *host, const char *metric, const char *value,
                       const char *state, unsigned int localtime, const char *tags_str, const char *description, unsigned int ttl)
@@ -417,48 +415,10 @@ send_data_to_riemann (const char *grid, const char *cluster, const char *host, c
    if (description)
       riemann_event_set_description(events[0], description);
 
-   debug_msg("tags = %s", tags_str);
-
    char **tags = NULL;
-/*
-   char *dup = strdup(tags_str);
-   char *last = strtok(dup, " ,");
-   char *last = strtok(tags_str, ",");
-
-   int i = 0;
-   while (last) {
-     i++;
-     debug_msg("tag%d = %s", i, last); 
-     tags = realloc(tags, sizeof (char **) * i);
-     tags[i - 1] = strdup(last);
-     last = strtok(NULL, ",");
-     debug_msg("tag%d = %s", i, tags[i]); 
-   }
-   debug_msg("marshalled %d tags", i);
-   debug_msg("tags => %s", tags[1]);
-   riemann_event_set_tags(events[0], (char **) tags, i - 1);
-*/
    int n_tags;
    tags = strtoknize(tags_str, strlen(tags_str) + 1, ",", 1, &n_tags);
    riemann_event_set_tags(events[0], (const char **)tags, n_tags);
-
-/*
-   const riemann_attribute_pairs_t attrs[] = {
-        { "grid", grid },
-        { "cluster", cluster },
-        { "environment", environment },
-        { "resource", host },
-   };
-   riemann_event_set_attributes(events[0], attrs, STATIC_ARRAY_SIZE(attrs));
-*/
-/*
-   riemann_attribute_pairs_t pairs[] = {
-     { "grid", grid },
-     { "cluster", cluster },
-     { "resource", resource }
-   };
-*/
-        // { "environment", environment },
 
    riemann_attribute_pairs_t *pairs = NULL;
    int n_attrs;
@@ -468,7 +428,9 @@ send_data_to_riemann (const char *grid, const char *cluster, const char *host, c
        char **pair = NULL;
        int i;
 
-       attrs = strtoknize(gmetad_config.riemann_attributes, strlen(gmetad_config.riemann_attributes) + 1, ",", 1, &n_attrs);
+       char attributes[512];
+       sprintf(attributes, "%s,grid=%s,cluster=%s,resource=%s", gmetad_config.riemann_attributes, grid, cluster, host);
+       attrs = strtoknize(attributes, strlen(attributes) + 1, ",", 1, &n_attrs);
        pairs = malloc(sizeof (riemann_attribute_pairs_t) * n_attrs);
        for (i = 0; i < n_attrs; i++) {
            pair = strtoknize(attrs[i], strlen(attrs[i]) + 1, "=", 1, NULL);
@@ -484,30 +446,48 @@ send_data_to_riemann (const char *grid, const char *cluster, const char *host, c
    riemann_event_set_ttl(events[0], ttl);
    riemann_message_set_events(&msg, events, 1);
 
+   char buffer[BUFSIZ];
+   static char *format = "%T,%h,%s,%d,%S,%mf,%md,%mi,%t,%G,%a";
+   error = riemann_event_strfevent(buffer, BUFSIZ, format, events[0]);
+   debug_msg(buffer);
+
    pthread_mutex_lock( &riemann_mutex );
    error = riemann_client_send_message(&riemann_cli, &msg, 0, NULL);
    pthread_mutex_unlock( &riemann_mutex );
 
    if (error)
-      debug_msg("Can't send message: %s", strerror(errno));
+      err_msg("Can't send message: %s", strerror(errno));
 
-   riemann_events_free(events, 1);
-
-   debug_msg("riemann_protocol = %s", gmetad_config.riemann_protocol);
    if (!strcmp(gmetad_config.riemann_protocol, "tcp"))
      {
-       resp = riemann_client_recv_message(&riemann_cli, 0, NULL);
+       resp = riemann_client_recv_message(&riemann_cli, MSG_WAITALL, NULL);
        if (!resp)
          {
-           debug_msg("riemann_client_recv_message() got no response\n");
+           err_msg("riemann_client_recv_message() got no response\n");
+           riemann_events_free(events, 1);
            return EXIT_FAILURE;
          }
        if (!resp->ok)
          {
-           debug_msg("riemann message error: %s\n", resp->error);
+           err_msg("riemann message error: %s\n", resp->error);
+           riemann_events_free(events, 1);
            return EXIT_FAILURE;
          }
+       for (int i = 0; i < resp->n_events; i++)
+         {
+           char buffer[BUFSIZ];
+           static char *format = "%T,%h,%s,%d,%S,%mf,%md,%mi,%t,%G,%a";
+           error = riemann_event_strfevent(buffer, BUFSIZ, format, resp->events[i]);
+           if (error)
+             {
+               err_msg("riemann_event_strfevent() error");
+               return EXIT_FAILURE;
+            }
+            debug_msg(buffer);
+        }
+        riemann_message_free(resp);
      }
+   riemann_events_free(events, 1);
    return EXIT_SUCCESS;
 
 }
